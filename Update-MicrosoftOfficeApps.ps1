169ed5d47aefd9e88ab81cfbdf3d4393b88e2686
CyberCondor <116996274+CyberCondor@users.noreply.github.com>
2023-03-16 19:39:27 -0700

    Init

---
+++ Update-MicrosoftOfficeApps.ps1
@@ -0,0 +1,7 @@
+﻿$OfficeC2RClientPath = "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe"
+if(Test-Path $OfficeC2RClientPath){
+   if(((Get-Item $OfficeC2RClientPath).VersionInfo.ProductName     -eq "Microsoft Office") -and 
+      ((Get-Item $OfficeC2RClientPath).VersionInfo.FileDescription -eq "Microsoft Office Click-to-Run Client")){
+       cmd /c '"C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user updatepromptuser=true forceappshutdown=false displaylevel=true'
+   }
+}
